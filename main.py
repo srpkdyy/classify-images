3276a34cf14252673c9c7843e204709e5b832108
srpkdyy <leader931334@outlook.jp>
2019-10-18 17:14:10 +0900

    Update diplay message

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     print('==> Device: ' + device)
     
     
-    print('==> Preparing dataset')
+    print('==> Preparing dataset..')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     
     train_ds = datasets.ImageFolder(
